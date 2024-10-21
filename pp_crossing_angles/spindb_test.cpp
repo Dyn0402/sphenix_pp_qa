757fac781542974f516d466534272bd281dabf1f
Dylan Neff <Dyn0402@hotmail.com>
2024-10-21 13:45:42 +0200

    spindb_test.cpp fixed many bugs

--- pp_crossing_angles/spindb_test.cpp
+++ pp_crossing_angles/spindb_test.cpp
@@ -5,11 +5,11 @@
 #include <uspin/SpinDBOutput.h>
 
 
-void PrintRunParameters(const SpinDBContent &spinContent, bool get_crossing_angle_info = false) {
-    std::cout << "Run Number: " << spinContent.GetRunNumber() << std::endl;
-    std::cout << "QA Level: " << spinContent.GetQALevel() << std::endl;
-    std::cout << "Fill Number: " << spinContent.GetFillNumber() << std::endl;
-    std::cout << "Bad Run: " << spinContent.GetBadRunFlag() << std::endl;
+void PrintRunParameters(SpinDBContent &spin_content, bool get_crossing_angle_info = false) {
+    std::cout << "Run Number: " << spin_content.GetRunNumber() << std::endl;
+    std::cout << "QA Level: " << spin_content.GetQALevel() << std::endl;
+    std::cout << "Fill Number: " << spin_content.GetFillNumber() << std::endl;
+    std::cout << "Bad Run: " << spin_content.GetBadRunFlag() << std::endl;
 
     // Get spin patterns
     int bluespin[120] = {0};
@@ -17,7 +17,7 @@ void PrintRunParameters(const SpinDBContent &spinContent, bool get_crossing_angl
     std::cout << "Blue spin pattern: [";
     for (int i = 0; i < 120; i++)
     {
-    bluespin[i] = spin_cont.GetSpinPatternBlue(i);
+    bluespin[i] = spin_content.GetSpinPatternBlue(i);
     std::cout << bluespin[i];
     if (i < 119)std::cout << ", ";
     }
@@ -26,17 +26,17 @@ void PrintRunParameters(const SpinDBContent &spinContent, bool get_crossing_angl
     std::cout << "Yellow spin pattern: [";
     for (int i = 0; i < 120; i++)
     {
-    yellspin[i] = spin_cont.GetSpinPatternYellow(i);
+    yellspin[i] = spin_content.GetSpinPatternYellow(i);
     std::cout << yellspin[i];
     if (i < 119)std::cout << ", ";
     }
     std::cout << "]" << std::endl;
 
     if (get_crossing_angle_info) {
-        std::cout << "Cross Angle: " << spinContent.GetCrossAngle() << std::endl;
-        std::cout << "Cross Angle Std: " << spinContent.GetCrossAngleStd() << std::endl;
-        std::cout << "Cross Angle Min: " << spinContent.GetCrossAngleMin() << std::endl;
-        std::cout << "Cross Angle Max: " << spinContent.GetCrossAngleMax() << std::endl;
+        std::cout << "Cross Angle: " << spin_content.GetCrossAngle() << std::endl;
+        std::cout << "Cross Angle Std: " << spin_content.GetCrossAngleStd() << std::endl;
+        std::cout << "Cross Angle Min: " << spin_content.GetCrossAngleMin() << std::endl;
+        std::cout << "Cross Angle Max: " << spin_content.GetCrossAngleMax() << std::endl;
     }
 }
 
@@ -48,13 +48,15 @@ int spindb_test() {
 
     for (int run_number : run_numbers) {
         SpinDBOutput dbOutput("phnxrc");
+        SpinDBContent spin_content;
+
         spin_out.StoreDBContent(run_number,run_number,qa_level);
-        spin_out.GetDBContentStore(spin_cont,run_number);
-        SpinDBContent spinContent;
+        spin_out.GetDBContentStore(spin_content,run_number);
+
         odbc::ResultSet *rs = dbOutput.GetResultSetForRun(run_number); // Assuming this method exists
         if (rs != nullptr) {
-            dbOutput.GetDBContent(spinContent, rs);
-            PrintRunParameters(spinContent, read_crossing_angle_info);
+            dbOutput.GetDBContent(spin_content, rs);
+            PrintRunParameters(spin_content, read_crossing_angle_info);
             delete rs;
         } else {
             std::cout << "Failed to retrieve data for run number: " << run_number << std::endl;
