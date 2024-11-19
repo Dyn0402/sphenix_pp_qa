a46c801c255480193770975c6268e2abee1449a0
Dylan Neff <Dyn0402@hotmail.com>
2024-11-19 18:30:22 +0100

    get_cni_polarizations.py now get user data from html, which contained additional information

--- cni_polarizations/get_cni_polarizations.py
+++ cni_polarizations/get_cni_polarizations.py
@@ -17,7 +17,13 @@ import pandas as pd
 
 
 def main():
-    url = 'https://www.cnipol.bnl.gov/fills/?rp=24&fn=&ft=&be=&mode=1&sb=Select'
+    # get_from_site_csv()
+    get_from_site_user()
+    print('donzo')
+
+
+def get_from_site_csv():
+    url = 'https://www.cnipol.bnl.gov/fills/?rp=24&fn=&ft=&be=&mode=11&sb=Select'
     reread = False  # If True, reread file even if it exists
     html_file_path = 'cnipol_fills.html'
     df_out_path = 'cnipol_fills.csv'
@@ -32,7 +38,6 @@ def main():
         print('HTML file not found')
 
     if not html_content or reread:
-
         # Need to set up a proxy to BNL internal. In a separate terminal run:
         # "ssh <usrname>@cssh.rhic.bnl.gov -L 3128:batch3.phy.bnl.gov:3128"
         proxy = {
@@ -60,7 +65,40 @@ def main():
     # Save the DataFrame to a CSV file
     df.to_csv(df_out_path, index=False)
 
-    print('donzo')
+
+def get_from_site_user():
+    url = 'https://www.cnipol.bnl.gov/fills/?rp=24&fn=&ft=&be=&mode=1&sb=Select'
+    reread = False  # If True, reread file even if it exists
+    html_file_path = 'cnipol_fills_user.html'
+    df_out_path = 'cnipol_fills_user.csv'
+
+    # Check if HTML file exists and read it if it does
+    try:
+        with open(html_file_path, 'r') as f:
+            html_content = f.read()
+            print('HTML read from file')
+    except FileNotFoundError:
+        html_content = None
+        print('HTML file not found')
+
+    if not html_content or reread:
+        # Need to set up a proxy to BNL internal. In a separate terminal run:
+        # "ssh <usrname>@cssh.rhic.bnl.gov -L 3128:batch3.phy.bnl.gov:3128"
+        proxy = {
+            "http": "http://localhost:3128",
+            "https": "http://localhost:3128"
+        }
+
+        # Fetch and print the HTML content
+        html_content = fetch_html(url, proxy=proxy)
+        save_html_to_file(html_content, html_file_path)
+
+    df = extract_user_data_to_dataframe(html_content)
+
+    df = convert_df_user(df)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(df_out_path, index=False)
 
 
 def fetch_html(url, proxy=None):
@@ -116,6 +154,89 @@ def extract_data_to_dataframe(input_string):
     return df
 
 
+def extract_user_data_to_dataframe(input_string):
+    """
+    Extract the data from the input string and return a DataFrame.
+    :param input_string:
+    :return:
+    """
+    start_flag = '<table class="simple cntr" cellspacing=0 align=center>'
+    end_flag = '<th colspan=10>&nbsp;<br>&nbsp;'
+
+    # Find the start and end of the table
+    start_idx = input_string.find(start_flag)
+    end_idx = input_string.find(end_flag)
+
+    # Extract the table content
+    table_content = input_string[start_idx:end_idx]
+    table_lines = table_content.split('\n')
+
+    headers, data, data_line = [], [], []
+    is_header = False
+    is_data = False
+    found_headers = False
+    blue_slope_error_code, yellow_slope_error_code = False, False
+    for line in table_lines:
+        if '<th ' in line:
+            is_data = False
+            is_header = True
+        elif '<tr class=my_tr_highlight>' in line:
+            is_header = False
+            is_data = True
+            if not found_headers and len(headers) > 0:
+                headers.append('Blue Slope Error')
+                headers.append('Yellow Slope Error')
+                found_headers = True
+            if len(data_line) > 0:
+                data_line.append(blue_slope_error_code)
+                data_line.append(yellow_slope_error_code)
+                data.append(data_line)
+                data_line = []
+                blue_slope_error_code, yellow_slope_error_code = False, False
+
+        if is_header:
+            if not found_headers:
+                if '<th ' in line:
+                    if 'colspan' in line:
+                        continue
+                    header = re.sub(r'<.*?>', '', line).strip()
+                    if 'bluPol' in line:
+                        header = 'Blue ' + header
+                    elif 'yelPol' in line:
+                        header = 'Yellow ' + header
+                    headers.append(header)
+                    if 'Blue' in header or 'Yellow' in header:
+                        headers.append(header + ' Error')
+        elif is_data:
+            if '<td ' in line:
+                line = re.sub(r'<!--.*?-->', '', line)  # Remove HTML comments
+                split_line = line.split('<td ')[1:]
+                split_line = [re.sub(r'<[^>]*>', '', '<td ' + item).strip() for item in split_line]
+                if 'Pol' in line:
+                    if len(split_line) != 1:
+                        print(f'Error: Expected 1 data column, found {len(split_line)}')
+                    if split_line[0] in ['', '&nbsp;']:
+                        data_line.append(pd.NA)
+                        data_line.append(pd.NA)
+                    else:
+                        val, err = split_line[0].split('&nbsp;&plusmn;&nbsp;')
+                        data_line.append(val)
+                        data_line.append(err)
+                    if "<span class='feature'>" in line:
+                        if 'bluPol' in line:
+                            blue_slope_error_code = True
+                        elif 'yelPol' in line:
+                            yellow_slope_error_code = True
+                        else:
+                            print(f'Error: Unexpected error code line: {line}')
+                else:
+                    data_line.extend(split_line)
+
+    df = pd.DataFrame(data, columns=headers)
+
+    return df
+
+
 def convert_df(df):
     """
     Convert the string values in the DataFrame to appropriate data types.
@@ -147,6 +268,41 @@ def convert_df(df):
             df[col + ' Error'] = pd.NA  # If there's no second part, assign NaN to the error column
 
     return df
+
+
+def convert_df_user(df):
+    """
+    Convert the string values in the DataFrame to appropriate data types.
+    :param df:
+    :return:
+    """
+    df['Fill'] = df['Fill'].astype(int)
+    df['Beam Energy, GeV'] = df['Beam Energy, GeV'].astype(float)
+    df['Physics On'] = pd.to_datetime(df['Physics On'], format='%b %d, %Y %H:%M:%S %a', errors='coerce')
+    df['Fill Length'] = df['Fill Length'].apply(time_to_seconds)
+
+    # For any column starting with Blue or Yellow convert to float
+    for col in df.columns:
+        if col.startswith('Blue') or col.startswith('Yellow'):
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    return df
+
+
+def time_to_seconds(time_str):
+    # Regular expression to capture hours, minutes, and seconds
+    time_parts = re.match(r'(\d+)h:(\d+)m:(\d+)s', time_str)
+
+    if time_parts:
+        hours = int(time_parts.group(1))
+        minutes = int(time_parts.group(2))
+        seconds = int(time_parts.group(3))
+
+        # Convert to seconds
+        total_seconds = hours * 3600 + minutes * 60 + seconds
+        return total_seconds
+    else:
+        return None  # Return None if the format doesn't match
 
 
 def calculate_average_polarizations(df):
